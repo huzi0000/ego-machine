8c1b341d2109a354896373906d71e57b1612e356
huzi0000 <huzi200449@gmail.com>
2026-06-13 11:40:00 +0500

    Fix wallet provider

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { SuiClientProvider, WalletProvider, createNetworkConfig } from '@mysten/dapp-kit'
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit'
 import '@mysten/dapp-kit/dist/index.css'
 
-const { networkConfig } = createNetworkConfig({
-  mainnet: { url: 'https://fullnode.mainnet.sui.io:443' },
-})
-
 const queryClient = new QueryClient()
+
+const networks = {
+  mainnet: { url: 'https://fullnode.mainnet.sui.io:443' },
+}
 
 export default function WalletProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
+      <SuiClientProvider networks={networks} defaultNetwork="mainnet">
         <WalletProvider autoConnect>
           {children}
         </WalletProvider>
